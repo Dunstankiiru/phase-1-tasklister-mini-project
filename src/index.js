ae668633bc59079020ab7e3d13873133e8878160
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-15 18:13:22 +0300

    completes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  selectTag()
-  let form = document.querySelector('form')
-  handleTask(form)
+  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target['new-task-description'].value)
+  })
 });
 
-
-function createNewTask(newTask, color) {
-
-  const li = document.createElement('li')
-  const btn = document.createElement('button')
-
-  deleteTask(btn)
-
-  btn.textContent = 'X'
-  li.textContent = `${newTask}  `
-  li.style = `color: ${color}`
-
-  document.querySelector('ul').appendChild(li).appendChild(btn)
+function createToDo(description) {
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  let listItems = document.createElement('li');
+  listItems.textContent = description;
+  listItems.appendChild(btn)
+  document.querySelector('#tasks').appendChild(listItems)
 }
-
-function deleteTask(element) {
-  element.addEventListener('click', (e) => {
-    e.target.parentNode.remove()
-  })
-}
-
-function handleTask(element) {
-  element.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const newTask = e.target['new-task-description'].value
-    const select = e.target[1]
-    const color = select.options[select.selectedIndex].value
-    console.log(color);
-    createNewTask(newTask, color)
-    element.reset()
-  })
-}
-
-function selectTag() {
-  const select = document.getElementById('colors')
-  const colors = ['', 'red', 'orange', 'green']
-  for (let color of colors) {
-    let option = document.createElement('option')
-    option.textContent = color
-    option.style = `background-color:${color}`
-    select.appendChild(option)
-  }
-  return select
+function handleDelete(e) {
+  e.target.parentNode.remove()
 }

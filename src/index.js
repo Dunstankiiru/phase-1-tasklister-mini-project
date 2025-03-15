ddc47f1854d63a0301b12f51cc7d899e20109ead
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-15 18:08:51 +0300

    completes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  const form = document.querySelector("#create-task-form")
-  const task = document.querySelector("#tasks")
-  console.log(tasks)
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const taskDescription = document.querySelector("#new-task-description")
-    const taskValue = taskDescription.value
-    if (taskValue == "") {
-      console.log(taskValue)
-      alert("Please write a to do")
-    }
-    else {
-      const taskList = document.createElement('li');
-      taskList.textContent = taskValue
-
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'x';
-      console.log(taskList)
-      tasks.appendChild(taskList)
-      taskList.appendChild(deleteButton)
-      deleteButton.addEventListener('click', () => {
-        taskList.remove();
-      })
-    }
-    form.reset()
-  });
-
-
+  selectTag()
+  let form = document.querySelector('form')
+  handleTask(form)
 });
+
+
+function createNewTask(newTask, color) {
+
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+
+  deleteTask(btn)
+
+  btn.textContent = 'X'
+  li.textContent = `${newTask}  `
+  li.style = `color: ${color}`
+
+  document.querySelector('ul').appendChild(li).appendChild(btn)
+}
+
+function deleteTask(element) {
+  element.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+}
+
+function handleTask(element) {
+  element.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTask = e.target['new-task-description'].value
+    const select = e.target[1]
+    const color = select.options[select.selectedIndex].value
+    console.log(color);
+    createNewTask(newTask, color)
+    element.reset()
+  })
+}
+
+function selectTag() {
+  const select = document.getElementById('colors')
+  const colors = ['', 'red', 'orange', 'green']
+  for (let color of colors) {
+    let option = document.createElement('option')
+    option.textContent = color
+    option.style = `background-color:${color}`
+    select.appendChild(option)
+  }
+  return select
+}
